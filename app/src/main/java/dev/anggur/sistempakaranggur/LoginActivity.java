85e35fd7a9bd77eef9e2bbc07757b6e70612bf92
iamabu <abugenzo27@gmail.com>
2018-07-02 22:44:24 +0700

    Tambah Login

--- app/src/main/java/dev/anggur/sistempakaranggur/LoginActivity.java
+++ app/src/main/java/dev/anggur/sistempakaranggur/LoginActivity.java
@@ -1,13 +1,31 @@
 package dev.anggur.sistempakaranggur;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edt_username)
+    EditText edtUsername;
+    @BindView(R.id.edt_password)
+    EditText edtPassword;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    public void onViewClicked() {
     }
 }
