67c647d8a9e7844ed5073b0b5aeef25407520b12
iamabu <abugenzo27@gmail.com>
2018-07-02 22:26:51 +0700

    initial commit

---
+++ app/src/main/java/dev/anggur/sistempakaranggur/DiagnosaActivity.java
@@ -0,0 +1,13 @@
+package dev.anggur.sistempakaranggur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiagnosaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnosa);
+    }
+}
