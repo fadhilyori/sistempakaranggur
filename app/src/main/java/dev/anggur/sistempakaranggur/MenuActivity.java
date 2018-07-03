308392e0077b247124266c9864e9546e652a20c7
iamabu <abugenzo27@gmail.com>
2018-07-04 00:03:48 +0700

    Add menu layout

---
+++ app/src/main/java/dev/anggur/sistempakaranggur/MenuActivity.java
@@ -0,0 +1,38 @@
+package dev.anggur.sistempakaranggur;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_daftar_diagnosa)
+    Button btnDaftarDiagnosa;
+    @BindView(R.id.btn_konsultasi)
+    Button btnKonsultasi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_daftar_diagnosa, R.id.btn_konsultasi})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_daftar_diagnosa:
+                startActivity(new Intent(MenuActivity.this,DiagnosaActivity.class));
+                break;
+            case R.id.btn_konsultasi:
+                startActivity(new Intent(MenuActivity.this,KonsultasiActivity.class));
+                break;
+        }
+    }
+}
