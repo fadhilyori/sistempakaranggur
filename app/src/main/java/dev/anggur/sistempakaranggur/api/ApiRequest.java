7e517adec1c418d725149a09f32bc86da84942a3
iamabu <abugenzo27@gmail.com>
2018-07-04 09:15:15 +0700

    Add API config

--- app/src/main/java/dev/anggur/sistempakaranggur/api/ApiRequest.java
+++ app/src/main/java/dev/anggur/sistempakaranggur/api/ApiRequest.java
@@ -1,8 +1,26 @@
 package dev.anggur.sistempakaranggur.api;
 
+import dev.anggur.sistempakaranggur.models.ResponseUser;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 /**
  * Created by Imam Abu Mansur on 04/07/2018.
  */
 
-public class ApiRequest {
+public interface ApiRequest {
+    @FormUrlEncoded
+    @Headers({"Accept: application/json"})
+    @POST("panggil.php?panggil=login")
+    Call<ResponseUser> login (@Field("email") String email,
+                                 @Field("password") String password);
+
+    @FormUrlEncoded
+    @Headers({"Accept: application/json"})
+    @POST("panggil.php?panggil=register")
+    Call<ResponseUser> register (@Field("name") String nama,
+                                 @Field("password") String password);
 }
--- app/src/main/java/dev/anggur/sistempakaranggur/api/RetroClient.java
+++ app/src/main/java/dev/anggur/sistempakaranggur/api/RetroClient.java
@@ -1,8 +1,27 @@
 package dev.anggur.sistempakaranggur.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Imam Abu Mansur on 04/07/2018.
  */
 
 public class RetroClient {
+    private static final String base_url = "http://10.208.129.21/sp-cf/";
+    private static Retrofit retrofit = null;
+
+    private static Retrofit getClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(base_url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static ApiRequest getRequestService(){
+        return getClient().create(ApiRequest.class);
+    }
 }
