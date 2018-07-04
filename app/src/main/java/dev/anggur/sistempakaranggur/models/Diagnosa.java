8ec0d106ff19209347b8b50db967f9cad0af4d6e
iamabu <abugenzo27@gmail.com>
2018-07-04 10:11:42 +0700

    Convert to Parcelable

--- app/src/main/java/dev/anggur/sistempakaranggur/models/Diagnosa.java
+++ app/src/main/java/dev/anggur/sistempakaranggur/models/Diagnosa.java
@@ -1,12 +1,15 @@
 package dev.anggur.sistempakaranggur.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Created by Imam Abu Mansur on 04/07/2018.
  */
 
-public class Diagnosa {
+public class Diagnosa implements Parcelable {
     private String kode_diagnosa;
     private String nama_diagnosa;
     private String keterangan;
@@ -34,4 +37,37 @@ public class Diagnosa {
     public ArrayList<Gejala> getGejala() {
         return gejala;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.kode_diagnosa);
+        dest.writeString(this.nama_diagnosa);
+        dest.writeString(this.keterangan);
+        dest.writeList(this.gejala);
+    }
+
+    protected Diagnosa(Parcel in) {
+        this.kode_diagnosa = in.readString();
+        this.nama_diagnosa = in.readString();
+        this.keterangan = in.readString();
+        this.gejala = new ArrayList<Gejala>();
+        in.readList(this.gejala, Gejala.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Diagnosa> CREATOR = new Parcelable.Creator<Diagnosa>() {
+        @Override
+        public Diagnosa createFromParcel(Parcel source) {
+            return new Diagnosa(source);
+        }
+
+        @Override
+        public Diagnosa[] newArray(int size) {
+            return new Diagnosa[size];
+        }
+    };
 }
--- app/src/main/java/dev/anggur/sistempakaranggur/models/Gejala.java
+++ app/src/main/java/dev/anggur/sistempakaranggur/models/Gejala.java
@@ -1,10 +1,13 @@
 package dev.anggur.sistempakaranggur.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Imam Abu Mansur on 04/07/2018.
  */
 
-public class Gejala {
+public class Gejala implements Parcelable {
     private String kode_gejala;
     private String nama_gejala;
     private String keterangan;
@@ -26,4 +29,34 @@ public class Gejala {
     public String getKeterangan() {
         return keterangan;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.kode_gejala);
+        dest.writeString(this.nama_gejala);
+        dest.writeString(this.keterangan);
+    }
+
+    protected Gejala(Parcel in) {
+        this.kode_gejala = in.readString();
+        this.nama_gejala = in.readString();
+        this.keterangan = in.readString();
+    }
+
+    public static final Parcelable.Creator<Gejala> CREATOR = new Parcelable.Creator<Gejala>() {
+        @Override
+        public Gejala createFromParcel(Parcel source) {
+            return new Gejala(source);
+        }
+
+        @Override
+        public Gejala[] newArray(int size) {
+            return new Gejala[size];
+        }
+    };
 }
